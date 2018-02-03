8544334ae5c8e6bdc7c3b3a76978a7fe92d4f201
mordaroso <mordaroso@gmail.com>
2018-02-03 19:02:09 -0400

    Delay fetching to every 30 seconds instead of every second for performance

--- webview.js
+++ webview.js
@@ -1,5 +1,20 @@
 module.exports = (Franz) => {
+  const fetchDelay = 30 * 1000; // 30 seconds
+  let lastFetch = 0;
+
+  // getMessages gets called looped and called every second by Franz
+  // This then waits and triggers every 30 seconds the fetchNotifications function
   const getMessages = function getMessages() {
+    if(lastFetch + fetchDelay < Date.now()) {
+      lastFetch = Date.now();
+      fetchNotifications();
+    }
+  };
+
+  // fetchNotifications initializes a GET AJAX call to http://github/notifications,
+  // parses the returned HTML to extract the number of unread notificaitons and
+  // returns the result to Franz.setBadge
+  const fetchNotifications = function fetchNotifications() {
     let request = new XMLHttpRequest();
     request.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
